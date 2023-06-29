b3bad856dac6bf1972b0f5e670d0d2bec55f671f
DeyKast <deykast@users.noreply.github.com>
2023-06-29 18:09:06 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,65 @@
-import Phonebook from './phonebook';
+import React, { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <Phonebook />
-    </div>
-  );
-};
+import ContactForm from './ContactFrom/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    const { contacts } = this.state;
+    const isDuplicate = contacts.some(
+      contact => contact.name === newContact.name
+    );
+
+    if (isDuplicate) {
+      alert(`${newContact.name} is already in contacts !`);
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  setFilter = filterValue => {
+    this.setState({ filter: filterValue });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <ContactForm addContact={this.addContact} />
+
+        <h2>Contacts</h2>
+        <Filter setFilter={this.setFilter} />
+        <ContactList
+          contacts={filteredContacts}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
